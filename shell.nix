e028d4c2e81621f4ecdab2d877678b36610d4b30
ruuffian <ruuffian@gmail.com>
2024-07-29 15:57:45 -0400

    Dev environment in shell.nix created. Directories + packages for front and backend initialized.

---
+++ shell.nix
@@ -0,0 +1,13 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    nodejs_22
+  ];
+  shellHook = ''
+    npm install
+    alias run="npm run"
+  '';
+}
+
