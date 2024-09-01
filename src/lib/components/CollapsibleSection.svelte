fd290d17e75612be360c9575875254bfd5862cdd
ruuffian <ruuffian@gmail.com>
2024-08-31 22:52:22 -0400

    Formatting for collapsible component

--- src/lib/components/CollapsibleSection.svelte
+++ src/lib/components/CollapsibleSection.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   // based on suggestions from:
   // Inclusive Components by Heydon Pickering https://inclusive-components.design/collapsible-sections/
   export let headerText;
@@ -8,7 +8,10 @@
 
 <div class="collapsible">
   <h3>
-    <button on:click={() => (expanded = !expanded)} class="rounded-xl p-2 m-4 bg-primary-500">
+    <button
+      on:click={() => (expanded = !expanded)}
+      class="rounded-xl p-2 m-4 bg-primary-500"
+    >
       <p class="text-surface-200">{headerText}</p>
       <!-- <svg viewBox="0 0 20 20" fill="none">
         <path class="vert" d="M10 1V19" stroke="black" stroke-width="2" />
@@ -17,7 +20,7 @@
     </button>
   </h3>
 
-  <div class="contents { expanded ? "" : "hidden"}">
+  <div class="contents {expanded ? '' : 'hidden'}">
     <slot />
   </div>
 </div>
