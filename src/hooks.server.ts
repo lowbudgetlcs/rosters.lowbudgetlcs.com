37ca58b02aaa30d26fb7224d159442ca6aae5826
ruuffian <ruuffian@gmail.com>
2024-08-31 21:22:15 -0400

    Migrated hooks

---
+++ src/hooks.server.ts
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+import { meta_db } from '$lib/server/database/db';
+import { users } from '$lib/server/database/metaSchema';
+import { eq } from 'drizzle-orm';
+import jwt from 'jsonwebtoken';
+import { initCron } from '$lib/server/cron';
+
+initCron();
+
+export const handle = async ({ event, resolve }) => {
+  const authCookie = event.cookies.get('AuthorizationToken');
+
+  if (authCookie) {
+    // Remove Bearer prefix
+    const token = authCookie.split(' ')[1];
+
+    try {
+      const jwtUser = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      if (typeof jwtUser === "string") {
+        throw new Error("Something went wrong");
+      }
+
+      const res = await meta_db.select().from(users).where(eq(users.id, jwtUser.id));
+
+      if (res.length == 0) {
+        throw new Error("User not found");
+      }
+      const user = res[0];
+
+      const sessionUser = {
+        id: user.id,
+        username: user.username,
+      };
+
+      event.locals.user = sessionUser;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return await resolve(event);
+};
